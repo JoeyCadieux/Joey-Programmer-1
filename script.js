a2239caad5d9aad6594c8787facca051d7c0e455
Joey Cadieux <joey@tech-center.com>
2020-04-24 15:21:23 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+console.warn("this is a Warning");
+console.warn("this is a warning");
+console.error("You made a mistake");
+console.error("You made a mistake");
+console.info("this is a info");
+console.info("this is a info");
+</script>
